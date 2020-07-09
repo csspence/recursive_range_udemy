1f66fe86b5dfc1d46751449819b92df100772cd4
Crew Spence <csspence123@gmail.com>
2020-07-09 06:28:10 -0500

    Wrote working solution.

--- recursive_range_udemy.js
+++ recursive_range_udemy.js
@@ -5,8 +5,8 @@ recursiveRange
 Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function.
 */
 
-function recursiveRange(){
-   
+const recursiveRange = (num) => {
+  return num > 0 ? num + recursiveRange(num - 1) : num;  
 }
 
 // recursiveRange(6) // 21
